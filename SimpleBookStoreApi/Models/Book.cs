c481d261cf038796c42927b2de745c721308dbe3
koskapan <koskapan@gmail.com>
2016-09-20 14:02:58 +0300

    Initial

---
+++ SimpleBookStoreApi/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBookStoreApi.Models
+{
+    public class Book
+    {
+        [JsonProperty("id")]
+        public int ID { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("author")]
+        public string Author { get; set; }
+
+        [JsonProperty("publicationYear")]
+        public int PublicationYear { get; set; }
+    }
+}
---
+++ SimpleBookStoreApi/Models/BooksContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBookStoreApi.Models
+{
+    public class BooksContext: DbContext
+    {
+        public BooksContext() : base("DefaultConnection") { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
